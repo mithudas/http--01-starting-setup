68143678b84c42f83a07565bbaeeb213ce860aea
Mithu Das <mithu.iitr@gmail.com>
2020-12-31 11:47:02 +0530

    Initial commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+import axios from 'axios';
+
+class Blog extends Component {
+state= {
+posts: [],
+selectedPostId:null
+}
+
+componentDidMount () {
+   axios.get("https://jsonplaceholder.typicode.com/posts")
+   .then (response =>{
+
+   const posts = response.data.slice(0,4);
+
+   const updatedPosts = posts.map(post=>{
+   return {
+   ...post,
+   author:'Max'
+   }
+   })
+   this.setState({posts: updatedPosts});
+   });
+}
+
+postSelectHandler=(id)=>{
+
+console.log("id>>>" , id);
+ this.setState({selectedPostId:id})
+}
+
+
+    render () {
+    const posts = this.state.posts
+    .map(post=>{
+    return <Post
+    key={post.id}
+    title={post.title}
+    clicked={()=>this.postSelectHandler(post.id)}
+    author={post.author}/>
+    });
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
